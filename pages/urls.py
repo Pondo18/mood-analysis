e64acd9ff8eae91645e77dfcd68623da0b9d8002
Moritz Moser <moritz@baseofm.de>
2022-06-28 15:03:11 +0200

    Add the functionality to upload an Image and submit it to the server

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('analyse_image', views.analyse_image, name='analyse-image'),
     path('analyse_recorded_video', views.analyse_recorded_video, name='analyse-recorded-video')
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,6 +8,12 @@ def index(request):
     return render(request, 'pages/index.html')
 
 
+def analyse_image(request):
+    if request.method == 'POST':
+        image = request.FILES['image']
+    return render(request, 'pages/index.html')
+
+
 def analyse_recorded_video(request):
     if request.method == 'POST':
         recorded_video = request.POST['recorded_video']
